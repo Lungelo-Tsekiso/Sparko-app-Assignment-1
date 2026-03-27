4e8ada18680aef868ce7bf82d5a9be3345d56cbe
Lungelo-Tsekiso <ST10533678@myemeris.edu.za>
2026-03-27 11:09:12 +0200

    initial code

---
+++ app/src/main/java/com/example/sparko/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.sparko
+
+import android.os.Bundle
+import android.view.View
+import android.view.ViewParent
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.SpinnerAdapter
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.insets.Protection
+
+class MainActivity : AppCompatActivity() {
+
+}
+
+
+
